97ab74db3e793a2ab9c244265bd2e495ab2ffa3f
Ilia Lutchenko <ilia.lutchenko@emlid.com>
2022-05-01 23:43:16 +0300

    main: init logger at sturtup

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,13 @@
-#include "esp_log.h"
+#include "logger.h"
 
 extern "C" {
 void app_main();
 }
 
-constexpr uint32_t main_task_delay_ms = 10;
-
 void app_main(void)
 {
-    ESP_LOGI("Main", "Motoheadset started");
+    Logger::init();
+    LOGI_TAGGED("Main", "Motoheadset started");
     /* Sheduler started automatically on boot,
         so we don't need to call vTaskStartScheduler()*/
 }
