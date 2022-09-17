a2037b60f47d0d090059103cfdf905350afbba70
Ilia Lutchenko <air_hamster@bk.ru>
2022-09-17 23:23:51 +0300

    cli: add base commands

---
+++ src/cli/console.cpp
@@ -0,0 +1,148 @@
+#include <stdio.h>
+#include <string.h>
+#include "esp_system.h"
+#include "esp_log.h"
+#include "esp_console.h"
+#include "esp_vfs_dev.h"
+#include "driver/uart.h"
+#include "linenoise/linenoise.h"
+#include "argtable3/argtable3.h"
+#include "console.h"
+
+#define TAG "CLI"
+#define PROMPT_STR "rx "
+
+TaskHandle_t TaskHandleCommandLine;
+namespace {
+constexpr auto PRIORITY = 15;
+constexpr auto STACK_SIZE = 10 * configMINIMAL_STACK_SIZE;
+constexpr auto TASK_CORE = 0;
+} // namespace
+
+static void initialize_console(void)
+{
+    /* Register commands */
+    esp_console_register_help_command();
+    register_system();
+
+    /* Drain stdout before reconfiguring it */
+    fflush(stdout);
+    fsync(fileno(stdout));
+
+    /* Disable buffering on stdin */
+    setvbuf(stdin, NULL, _IONBF, 0);
+
+    /* Minicom, screen, idf_monitor send CR when ENTER key is pressed */
+    esp_vfs_dev_uart_port_set_rx_line_endings(CONFIG_ESP_CONSOLE_UART_NUM,
+                                              ESP_LINE_ENDINGS_CR);
+    /* Move the caret to the beginning of the next line on '\n' */
+    esp_vfs_dev_uart_port_set_tx_line_endings(CONFIG_ESP_CONSOLE_UART_NUM,
+                                              ESP_LINE_ENDINGS_CRLF);
+
+    /* Configure UART. Note that REF_TICK is used so that the baud rate remains
+     * correct while APB frequency is changing in light sleep mode.
+     */
+    const uart_config_t uart_config = {
+        .baud_rate = CONFIG_ESP_CONSOLE_UART_BAUDRATE,
+        .data_bits = UART_DATA_8_BITS,
+        .parity = UART_PARITY_DISABLE,
+        .stop_bits = UART_STOP_BITS_1,
+        .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
+        .rx_flow_ctrl_thresh = 120, // should be less than UART_FIFO_LEN (128)
+        .source_clk = UART_SCLK_REF_TICK,
+    };
+    /* Install UART driver for interrupt-driven reads and writes */
+    ESP_ERROR_CHECK(uart_driver_install(CONFIG_ESP_CONSOLE_UART_NUM, 256, 0, 0, NULL, 0));
+    ESP_ERROR_CHECK(uart_param_config(CONFIG_ESP_CONSOLE_UART_NUM, &uart_config));
+
+    /* Tell VFS to use UART driver */
+    esp_vfs_dev_uart_use_driver(CONFIG_ESP_CONSOLE_UART_NUM);
+
+    /* Initialize the console */
+    esp_console_config_t console_config = {
+        .max_cmdline_length = 256,
+        .max_cmdline_args = 8,
+#if CONFIG_LOG_COLORS
+        .hint_color = atoi(LOG_COLOR_CYAN),
+        .hint_bold = 1
+#endif
+    };
+    ESP_ERROR_CHECK(esp_console_init(&console_config));
+
+    /* Configure linenoise line completion library */
+    /* Enable multiline editing. If not set, long commands will scroll within
+     * single line.
+     */
+    linenoiseSetMultiLine(1);
+
+    /* Tell linenoise where to get command completions and hints */
+    linenoiseSetCompletionCallback(&esp_console_get_completion);
+    linenoiseSetHintsCallback((linenoiseHintsCallback*)&esp_console_get_hint);
+
+    /* Set command history size */
+    linenoiseHistorySetMaxLen(100);
+
+    /* Don't return empty lines */
+    linenoiseAllowEmpty(false);
+}
+
+void CommandLineTask(void* argument)
+{
+    const char* prompt = LOG_COLOR_I PROMPT_STR "> " LOG_RESET_COLOR;
+
+    /* Figure out if the terminal supports escape sequences */
+    int probe_status = linenoiseProbe();
+    if (probe_status) { /* zero indicates success */
+        printf("\n"
+               "Your terminal application does not support escape sequences.\n"
+               "Line editing and history features are disabled.\n"
+               "On Windows, try using Putty instead.\n");
+        linenoiseSetDumbMode(1);
+        prompt = PROMPT_STR "> ";
+    }
+
+    /* Main loop */
+    while (true) {
+        /* Get a line using linenoise.
+         * The line is returned when ENTER is pressed.
+         */
+        char* line = linenoise(prompt);
+        if (line != NULL) { /* Break on EOF or error */
+            /* Add the command to the history if not empty*/
+            if (strlen(line) > 0) {
+                linenoiseHistoryAdd(line);
+            }
+
+            /* Try to run the command */
+            int ret;
+            esp_err_t err = esp_console_run(line, &ret);
+            if (err == ESP_ERR_NOT_FOUND) {
+                printf("Unrecognized command\n");
+            } else if (err == ESP_ERR_INVALID_ARG) {
+                // command was empty
+                printf("Invalid arguments\n");
+            } else if (err == ESP_OK && ret != ESP_OK) {
+                printf("Command returned non-zero error code: 0x%x (%s)\n", ret,
+                       esp_err_to_name(ret));
+            } else if (err != ESP_OK) {
+                printf("Internal error: %s\n", esp_err_to_name(err));
+            }
+            /* linenoise allocates line buffer on the heap, so need to free it */
+            linenoiseFree(line);
+        }
+    }
+}
+
+void cli_start(void)
+{
+    initialize_console();
+    xTaskCreatePinnedToCore(CommandLineTask, "CLI", STACK_SIZE, NULL, PRIORITY,
+                            &TaskHandleCommandLine, TASK_CORE);
+}
+
+void cli_stop(void)
+{
+    ESP_LOGE(TAG, "Terminating console");
+    vTaskDelete(&TaskHandleCommandLine);
+    esp_console_deinit();
+}
