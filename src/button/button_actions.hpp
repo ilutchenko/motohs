9a8251b0b8e89c1fa50fc610352affd1bcafd7bc
Ilia Lutchenko <air_hamster@bk.ru>
2022-10-01 22:18:26 +0300

    button add button class sources

---
+++ src/button/button_actions.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+namespace MotoHS {
+    namespace Controllers {
+        enum class ButtonActions
+        {
+            None,
+            ShortClick,
+            LongPress,
+            LongClick
+        };
+    }
+} // namespace MotoHS
