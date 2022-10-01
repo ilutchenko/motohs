aab3bf2da3c85868c84fb47337259cbba539a223
Ilia Lutchenko <air_hamster@bk.ru>
2022-10-01 22:27:12 +0300

    main: add button handler

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,10 @@
-#include "logger.h"
-#include "console.h"
-#include "system_task/system_task.hpp"
+#include <logging/logger.h>
+#include <cli/console.h>
+#include <system_task/system_task.hpp>
+#include <button/button.hpp>
 
-MotoHS::System::SystemTask system_task;
+MotoHS::Controllers::ButtonHandler button_handler(GPIO_NUM_0);
+MotoHS::System::SystemTask system_task(button_handler);
 
 extern "C" {
 void app_main();
@@ -12,7 +14,7 @@ void app_main(void)
 {
     Logger::init();
     cli_start();
-    system_task.start();
+    system_task.Start();
     LOGI_TAGGED("Main", "Motoheadset started");
     /* Sheduler started automatically on boot,
         so we don't need to call vTaskStartScheduler()*/
